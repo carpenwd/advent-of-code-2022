3dc59d57fe0fa4e73fab90cdb15510e6990bf013
wcarpenter <50086214+hammerheim7146@users.noreply.github.com>
2022-12-01 19:08:17 +0000

    Linting and README updates

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # advent-of-code-2022
+* Day 1:
+  * Part 1: Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+  * Part 2: Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
--- day-1/calorie_counting.py
+++ day-1/calorie_counting.py
@@ -1,9 +1,7 @@
 """
 https://adventofcode.com/2022/day/1
 
-Part 1: Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
-Part 2: Find the top three Elves carrying the most Calories. 
-        How many Calories are those Elves carrying in total?
+
 """
 with open('day-1/calorie_input.txt', 'r', encoding="utf-8") as calorie_file:
     ELF_COUNT = 1
